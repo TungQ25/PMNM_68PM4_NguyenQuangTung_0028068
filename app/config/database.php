7a7bed5bdc613457211489e378add166782f6094
TungQ25 <qtung.work@gmail.com>
2026-05-27 16:02:02 +0700

    Add database setup and configuration files Create a new SQL file

---
+++ app/config/database.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'host' => 'localhost',
+    'database' => 'ql_sinhvien',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8mb4', // Dùng bảng mã utf8mb4 để lưu và đọc chữ tiếng việt
+];
